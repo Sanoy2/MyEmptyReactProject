2cf220f190591515041cfecf3608fd139e4afb51
Sanoy2 <krzysztof.tomkow@gmail.com>
2020-10-31 14:53:46 +0100

    Some basic components, strings file, basic routing

---
+++ src/app/themes/theme.js
@@ -0,0 +1,16 @@
+  
+import { createMuiTheme } from "@material-ui/core/styles";
+import indigo from "@material-ui/core/colors/indigo";
+import orange from "@material-ui/core/colors/orange";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: orange
+  },
+  status: {
+    danger: "orange",
+  },
+});
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./app/themes/theme";
+import { SnackbarProvider } from 'notistack';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={10}>
+        <App />
+      </SnackbarProvider>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
