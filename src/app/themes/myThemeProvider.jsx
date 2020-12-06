ea191880075c8e3da1c572e662c806beecf63105
ktomkow <krzysztof.tomkow@gmail.com>
2020-12-06 13:53:48 +0100

    Removed unused things from theme provider

--- src/app/themes/myThemeProvider.jsx
+++ src/app/themes/myThemeProvider.jsx
@@ -1,31 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import { ThemeProvider } from '@material-ui/core/styles';
-import { Button } from '@material-ui/core';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
-  currentTheme,
-  availableThemes,
-  switchTheme,
+  currentTheme
 } from '../redux/slices/themeSlice';
 
 const MyThemeProvider = (props) => {
   const theme = useSelector(currentTheme);
-  const allThemes = useSelector(availableThemes);
-  const dispatch = useDispatch();
 
   return (
     <ThemeProvider theme={theme.theme}>
-      <p>Current theme: {theme.name}</p>
-      <div>
-        {allThemes.map((item) => (
-          <div key={Math.random()}>
-            <Button onClick={() => dispatch(switchTheme(item))}>
-              {item}
-            </Button>
-          </div>
-        ))}
-      </div>
       {props.children}
     </ThemeProvider>
   );
